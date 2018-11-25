d5c08dcdb5c79b8a3673826d87b29d014cb0df80
Markus Appel <markospoparkos@gmail.com>
2018-11-25 10:25:53 +0100

    Refactor: make button a component

--- src/components/GameControls.vue
+++ src/components/GameControls.vue
@@ -5,11 +5,9 @@
     <Counter label="Score" :num="score" />
     <Counter label="Multiplier" :num="scoreMultiplier" />
 
-    <button type="button" @click="game_over ? restart() : pauseInput()">
-      <span>
-        {{ this.game_over ? 'Restart' : this.paused ? 'Resume' : 'Pause' }}
-      </span>
-    </button>
+    <SidebarButton @click="gameOver ? restart() : pauseInput()">
+      {{ this.gameOver ? 'Restart' : this.paused ? 'Resume' : 'Pause' }}
+    </SidebarButton>
 
   </div>
 </template>
@@ -17,10 +15,11 @@
 <script>
 import comms from '@/vuePhaserComms'
 import Counter from './Counter'
+import SidebarButton from './SidebarButton'
 
 export default {
   name: 'gameControls',
-  components: { Counter },
+  components: { Counter, SidebarButton },
   data () {
     return {
       num_lives: 0,
@@ -61,28 +60,6 @@ export default {
 
   > :not(:first-child) {
     margin-top: 1rem;
-  }
-
-  > button {
-    border: 1px solid darkgrey;
-    border-radius: 0.25rem;
-    background-color: white;
-    padding: 0.375rem;
-    cursor: pointer;
-    transition: box-shadow 0.15s;
-
-    > span {
-      display: inline-block;
-      transition: transform 0.15s;
-    }
-
-    &:hover, &:active {
-      box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.2);
-
-      > span {
-        transform: scale(1.15);
-      }
-    }
   }
 }
 </style>
---
+++ src/components/SidebarButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button type="button" v-on="$listeners">
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'sidebarButton'
+}
+</script>
+
+<style lang="scss" scoped>
+  button {
+    border: 1px solid darkgrey;
+    border-radius: 0.25rem;
+    background-color: white;
+    padding: 0.375rem;
+    cursor: pointer;
+    transition: box-shadow 0.15s;
+
+    > span {
+      display: inline-block;
+      transition: transform 0.15s;
+    }
+
+    &:hover, &:active {
+      box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.2);
+
+      > span {
+        transform: scale(1.15);
+      }
+    }
+  }
+</style>
