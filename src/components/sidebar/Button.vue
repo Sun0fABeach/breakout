e03e514baf20e0bea1ce19a559a3daa6ad265e55
Markus Appel <markospoparkos@gmail.com>
2018-11-26 20:34:18 +0100

    Refactor: style indentation

--- src/components/sidebar/Button.vue
+++ src/components/sidebar/Button.vue
@@ -13,25 +13,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  button {
-    border: 1px solid darkgrey;
-    border-radius: 0.25rem;
-    background-color: white;
-    padding: 0.375rem;
-    cursor: pointer;
-    transition: box-shadow 0.15s;
+button {
+  border: 1px solid darkgrey;
+  border-radius: 0.25rem;
+  background-color: white;
+  padding: 0.375rem;
+  cursor: pointer;
+  transition: box-shadow 0.15s;
+
+  > span {
+    display: inline-block;
+    transition: transform 0.15s;
+  }
+
+  &:hover, &:active {
+    box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.2);
 
     > span {
-      display: inline-block;
-      transition: transform 0.15s;
-    }
-
-    &:hover, &:active {
-      box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.2);
-
-      > span {
-        transform: scale(1.15);
-      }
+      transform: scale(1.15);
     }
   }
+}
 </style>
