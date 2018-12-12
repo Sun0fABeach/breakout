e12d3e76dac671cc37484b4bfcd6ab083a82e15f
Markus Appel <markospoparkos@gmail.com>
2018-12-12 22:38:36 +0100

    Refactor: move anim keyframes to animate.scss

--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -54,70 +54,14 @@ button {
   cursor: pointer;
 }
 
-@keyframes bounceInRight {
-  from,
-  60%,
-  75%,
-  90%,
-  to {
-    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-  from {
-    opacity: 0;
-    transform: translate3d(500px, 0, 0);
-  }
-  60% {
-    opacity: 1;
-    transform: translate3d(-25px, 0, 0);
-  }
-  75% {
-    transform: translate3d(10px, 0, 0);
-  }
-  90% {
-    transform: translate3d(-5px, 0, 0);
-  }
-  to {
-    transform: translate3d(0, 0, 0);
-  }
-}
-
 .bounceInRight {
   animation-delay: 300ms;
   animation-name: bounceInRight;
 }
 
-@keyframes bounceOut {
-  20% {
-    transform: scale3d(0.9, 0.9, 0.9);
-  }
-  50%,
-  55% {
-    opacity: 1;
-    transform: scale3d(1.1, 1.1, 1.1);
-  }
-  to {
-    opacity: 0;
-    transform: scale3d(0.3, 0.3, 0.3);
-  }
-}
-
 .bounceOut {
   animation-duration: 0.75s;
   animation-name: bounceOut;
-}
-
-@keyframes pulse {
-  from {
-    transform: scale3d(1, 1, 1);
-  }
-
-  50% {
-    transform: scale3d(1.05, 1.05, 1.05);
-  }
-
-  to {
-    transform: scale3d(1, 1, 1);
-  }
 }
 
 .pulse {
