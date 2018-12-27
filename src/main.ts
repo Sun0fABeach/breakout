b9795421ab02ffe97633bb4cc043686b094501f3
Markus Appel <markospoparkos@gmail.com>
2018-12-27 10:42:48 +0100

    Install vuex

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import './animate.scss'
 
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 // eslint-disable-next-line
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
