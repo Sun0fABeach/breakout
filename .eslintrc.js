2b79168181d90c477f4cd8143e5576aa2a6b7a08
Markus Appel <markospoparkos@gmail.com>
2018-12-28 11:12:43 +0100

    Disable some annoying eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 'off',
+    'no-multiple-empty-lines': 'off',
+    'eol-last': 'off'
   },
   parserOptions: {
     parser: 'typescript-eslint-parser'
