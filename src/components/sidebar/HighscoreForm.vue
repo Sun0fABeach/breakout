7a4f03b9b34eff927df3578f1f2f8a85c7e6eb1a
Markus Appel <markospoparkos@gmail.com>
2019-02-03 12:02:52 +0100

    Refactor: highscore form becomes component

---
+++ src/components/sidebar/HighscoreForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form id="sidebar-hs-form" @submit.prevent.stop>
+    <label>
+      Your name
+      <input
+        type="text"
+        name="user_name"
+        maxlength="16"
+        v-model.trim="name"
+        @keyup.enter="submit"
+        ref="input"
+      />
+    </label>
+    <SidebarButton class="submit-button" @click="submit">
+      Submit Score
+    </SidebarButton>
+  </form>
+</template>
+
+<script>
+import SidebarButton from './Button'
+
+export default {
+  name: 'sidebar-hs-form',
+  components: { SidebarButton },
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    submit () {
+      if (this.name) {
+        this.$emit('submit', this.name)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#sidebar-hs-form {
+  label {
+    color: dimgrey;
+  }
+  input {
+    width: 100%;
+    margin-top: 0.375rem;
+    padding: 0.125rem;
+  }
+}
+</style>
+
+<style lang="scss">
+/* we need use non-scoped scss to style the submit button since it is a
+ * functional component and vue is not able to apply scoped styles to such
+ * components...
+ */
+#sidebar-hs-form .submit-button {
+  width: 100%;
+  margin-top: 0.75rem;
+}
+</style>
--- src/components/sidebar/HighscoreOverlay.vue
+++ src/components/sidebar/HighscoreOverlay.vue
@@ -5,34 +5,18 @@
         <span>Final Score</span>
         <span>{{ displayedScore }}</span>
       </div>
-
-      <form @submit.prevent.stop>
-        <label>
-          Your name
-          <input
-            type="text"
-            name="user_name"
-            maxlength="16"
-            v-model="name"
-            @keyup.enter="submit"
-            ref="input"
-          />
-        </label>
-        <SidebarButton class="submit-button" @click="submit">
-          Submit Score
-        </SidebarButton>
-      </form>
+      <HighscoreForm @submit="submit" ref="form" />
     </div>
   </transition>
 </template>
 
 <script>
-import SidebarButton from './Button'
+import HighscoreForm from './HighscoreForm'
 import { mapMutations } from 'vuex'
 
 export default {
   name: 'sidebarHighscoreOverlay',
-  components: { SidebarButton },
+  components: { HighscoreForm },
   props: {
     open: {
       type: Boolean,
@@ -45,8 +29,7 @@ export default {
   },
   data () {
     return {
-      displayedScore: 0, // set in watcher
-      name: ''
+      displayedScore: 0 // set in watcher
     }
   },
   watch: {
@@ -62,16 +45,11 @@ export default {
   methods: {
     focusInput () {
       if (this.open) {
-        this.$refs.input.focus()
+        this.$refs.form.$refs.input.focus()
       }
     },
-    submit () {
-      if (this.name) {
-        this.addHighscore({
-          name: this.name,
-          score: this.score
-        })
-      }
+    submit (name) {
+      this.addHighscore({ name, score: this.score })
     },
     ...mapMutations([
       'addHighscore'
@@ -113,28 +91,8 @@ export default {
     }
   }
 
-  form {
-    margin-top: 1.25rem;
-
-    label {
-      color: dimgrey;
-    }
-    input {
-      width: 100%;
-      margin-top: 0.375rem;
-      padding: 0.125rem;
-    }
+  > #sidebar-hs-form {
+      margin-top: 1.25rem;
   }
-}
-</style>
-
-<style lang="scss">
-/* we need use non-scoped scss to style the submit button since it is a
- * functional component and vue is not able to apply scoped styles to such
- * components...
- */
-#sidebar-hs-overlay .submit-button {
-  width: 100%;
-  margin-top: 0.75rem;
 }
 </style>
